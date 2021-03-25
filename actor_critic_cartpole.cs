832a69cac2e020f536b0ebdaefad272d1b6cca88
Samuel Caldas <samuel.caldas@gmail.com>
2021-03-25 05:11:44 -0300

    Update actor_critic_cartpole.cs

--- actor_critic_cartpole.cs
+++ actor_critic_cartpole.cs
@@ -97,30 +97,30 @@ var num_actions = 2;
 var num_hidden = 128;
 
 var layers = keras.layers;
-inputs = layers.Input(shape: (num_inputs,));
-common = layers.Dense(num_hidden, activation: "relu").Apply(inputs);
-action = layers.Dense(num_actions, activation: "softmax").Apply(common);
-critic = layers.Dense(1).Apply(common);
+var inputs = layers.Input(shape: (num_inputs,));
+var common = layers.Dense(num_hidden, activation: "relu").Apply(inputs);
+var action = layers.Dense(num_actions, activation: "softmax").Apply(common);
+var critic = layers.Dense(1).Apply(common);
 
-model = keras.Model(inputs: inputs, outputs:[action, critic]);
+var model = keras.Model(inputs: inputs, outputs:new[] { action, critic });
 
 /*/
 //// Train
 /*/
 
-optimizer = keras.optimizers.Adam(learning_rate: 0.01);
-huber_loss = keras.losses.Huber();
-action_probs_history = new List<double>();
-critic_value_history = new List<double>();
-rewards_history = new List<double>();
-running_reward = 0;
-episode_count = 0;
+var optimizer = keras.optimizers.Adam(learning_rate: 0.01);
+var huber_loss = keras.losses.Huber();
+var action_probs_history = new List<double>();
+var critic_value_history = new List<double>();
+var rewards_history = new List<double>();
+var running_reward = 0;
+var episode_count = 0;
 
 while (true)  // Run until solved
-    state = env.Reset();
+    var state = env.Reset();
     episode_reward = 0;
     using (var tape = tf.GradientTape()) {
-        for (int timestep in range(1, max_steps_per_episode)){
+        for (int timestep <= 1, max_steps_per_episode){
             // env.render(); Adding this line would show the attempts
             // of the agent in a pop up window.
 
@@ -139,7 +139,7 @@ while (true)  // Run until solved
             // Apply the sampled action in our environment
             var (state, reward, done, _) = env.Step(action);
             rewards_history.Add(reward);
-            episode_reward += reward;
+            var episode_reward += reward;
 
             if (done)
                 break;
@@ -151,8 +151,8 @@ while (true)  // Run until solved
         // - At each timestep what was the total reward received after that timestep
         // - Rewards in the past are discounted by multiplying them using (gamma
         // - These are the labels for (int our critic
-        returns = new List<double>();
-        discounted_sum = 0;
+        var returns = new List<double>();
+        var discounted_sum = 0;
         for (int r in rewards_history["::-1"]) {
             discounted_sum = r + gamma * discounted_sum;
             returns.insert(0, discounted_sum);
@@ -160,19 +160,19 @@ while (true)  // Run until solved
         // Normalize
         returns = np.array(returns);
         returns = (returns - np.mean(returns)) / (np.std(returns) + eps);
-        returns = returns.tolist();
+        returns = returns.ToList();
 
         // Calculating loss values to update our network
-        history = zip(action_probs_history, critic_value_history, returns);
-        actor_losses = new List<double>();
-        critic_losses = new List<double>();
+        var history = zip(action_probs_history, critic_value_history, returns);
+        var actor_losses = new List<double>();
+        var critic_losses = new List<double>();
         for (int log_prob, value, ret in history) {
             // At this point in history, the critic estimated that we would get a
             // total reward = `value` in the future. We took an action using (log probability
             // of `log_prob` and ended up recieving a total reward = `ret`.
             // The actor must be updated so that it predicts an action that leads to
             // high rewards (compared to critic"s estimate) using (high probability.
-            diff = ret - value;
+            var diff = ret - value;
             actor_losses.Add(-log_prob * diff);  // actor loss
 
             // The critic must be updated so that it predicts a better estimate of
@@ -180,8 +180,8 @@ while (true)  // Run until solved
             critic_losses.Add(huber_loss(tf.expand_dims(value, 0), tf.expand_dims(ret, 0)));
         }
         // Backpropagation
-        loss_value = sum(actor_losses) + sum(critic_losses);
-        grads = tape.gradient(loss_value, model.trainable_variables);
+        var loss_value = sum(actor_losses) + sum(critic_losses);
+        var grads = tape.gradient(loss_value, model.trainable_variables);
         optimizer.apply_gradients(zip(grads, model.trainable_variables));
 
         // Clear the loss and reward history
@@ -192,7 +192,7 @@ while (true)  // Run until solved
     // Log details
     episode_count += 1;
     if (episode_count % 10 == 0) {
-        template = "running reward: {:.2f} at episode {}";
+        var template = "running reward: {:.2f} at episode {}";
         Console.WriteLine(template.format(running_reward, episode_count));
     }
     if (running_reward > 195){  // Condition to consider the task solved
